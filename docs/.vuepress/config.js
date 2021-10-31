27a5c427ed02a34475499b8f90d536c9edf7bc59
grant <1191945756@qq.com>
2021-10-31 21:26:11 +0800

    auto deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  base: '/docs/',
+  title: 'grant\'s tech docs',
+  description: 'coding docs',
+  markdown: {
+    lineNumbers: true,
+  },
+  evergreen: true,
+  themeConfig: {
+    repo: 'https://github.com/grant-ymy/docs',
+    repoLabel: '源码',
+    lastUpdated: '上次更新',
+    editLinks: true,
+    editLinkText: '编辑此页内容',
+    nav: [
+      { text: '首页', link: '/' },
+    ],
+    sidebar: [
+      ['/', '简介'],
+      {
+        title: 'PHP',
+        sidebarDepth: 2,
+        children: [
+          'php/',
+        ],
+      },
+      {
+        title: 'MySQL',
+        sidebarDepth: 2,
+        children: [
+          'mysql/',
+        ],
+      },
+      {
+        title: 'Laravel',
+        sidebarDepth: 2,
+        children: [
+          'laravel/',
+        ],
+      },
+    ]
+  }
+}
