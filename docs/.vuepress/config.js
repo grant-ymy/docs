388ffbf7c75cf80a73faf82d0c5b2571ba720f23
grant <1191945756@qq.com>
2021-11-07 22:24:56 +0800

    auto deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,12 +7,34 @@ module.exports = {
     lineNumbers: true,
   },
   evergreen: true,
+  plugins: {
+    '@vuepress/back-to-top': true,
+    '@vuepress/active-header-links': {
+      sidebarLinkSelector: '.sidebar-link',
+      headerAnchorSelector: '.header-anchor'
+    },
+    '@vssue/vuepress-plugin-vssue': {
+      platform: 'github-v4', //v3的platform是github，v4的是github-v4
+      locale: 'zh', //语言
+      // 其他的 Vssue 配置
+      owner: 'grant-ymy', //github账户名
+      repo: 'https://github.com/grant-ymy/docs', //github一个项目的名称
+      clientId: '1eab3284f097e9b1d067',//注册的Client ID
+      clientSecret: '285d5f1a0a443580a778dd0cbc23a3d990070ed0',//注册的Client Secret
+      autoCreateIssue: true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+    },
+  },
   themeConfig: {
+    logo: '/head.png',
     repo: 'https://github.com/grant-ymy/docs',
-    repoLabel: '源码',
-    lastUpdated: '上次更新',
+    docsDir: 'docs',
+    docsBranch: 'main',
     editLinks: true,
-    editLinkText: '编辑此页内容',
+    repoLabel: '仓库',
+    lastUpdated: '上次更新时间',
+    editLinks: true,
+    editLinkText: '帮助我完善此内容',
+    smoothScroll: true,
     nav: [
       { text: '首页', link: '/' },
     ],
@@ -46,7 +68,7 @@ module.exports = {
         ],
       },
       {
-        title: 'go',
+        title: 'Golang',
         sidebarDepth: 2,
         children: [
           'go/',
@@ -60,10 +82,17 @@ module.exports = {
         ],
       },
       {
-        title: 'docker',
+        title: 'Docker',
         sidebarDepth: 2,
         children: [
           'docker/',
+        ],
+      },
+      {
+        title: 'Git',
+        sidebarDepth: 2,
+        children: [
+          'git/',
         ],
       },
       {
